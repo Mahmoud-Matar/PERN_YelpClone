d1ec6fe13e711f90b123ff073d3e6accc1894a75
Mahmoud-Matar <mahmoud_gamal2001@aucegypt.edu>
2024-03-15 02:31:49 -0800

    implemented BackEnd

--- server/server.js
+++ server/server.js
@@ -1,39 +1,130 @@
 require('dotenv').config()
 
 const express = require('express')
+const query = require('./db')
 
 const app = express()
 
 app.use(express.json())
 
 
-app.get('/api/v1/restaurants',(req,res) => {
-    res.status(200).json({msg: "get all restaurants"})
+app.get('/api/v1/restaurants', async (req,res) => 
+{
+    try 
+    {
+        const results = await query('SELECT * FROM restaurants')
+
+        const restaurants = results.rows
+
+        res.status(200).json({
+            status:'success',
+            restaurantsNum: restaurants.length,
+            data: {restaurants}
+        })
+        
+    } 
+    catch (error) 
+    {
+        console.log(error)
+        
+    }
 })
 
-app.get('/api/v1/restaurants/:id',(req,res) => 
+app.get('/api/v1/restaurants/:id', async (req,res) => 
 {
-    const {id:restaurantsID} = req.params
-    res.status(200).json({msg: `get restaurants with id ${restaurantsID}`})
+    try 
+    {
+        const {id:restaurantsID} = req.params
+
+        const result = await query(`SELECT * FROM restaurants WHERE id = $1`,[restaurantsID])
+
+        const restaurant = result.rows
+
+        res.status(200).json({
+            status:'success',
+            restaurantsNum: restaurant.length,
+            data: {restaurant}
+        })
+        
+    } 
+    catch (error) 
+    {
+        console.log(error)
+        
+    }
 })
 
-app.post('/api/v1/restaurants',(req,res) => 
+app.post('/api/v1/restaurants', async (req,res) => 
 {
-    const {body} = req
-    res.status(201).json({body})
+    try 
+    {
+        const {name,location,price_range} = req.body
+
+        if(!name || !location || !price_range)
+            throw new Error("Bad Request");
+
+        const results = await query('INSERT INTO restaurants (name,location,price_range) VALUES ($1,$2,$3) RETURNING *',
+        [name,location,price_range])
+
+        const restaurant = results.rows[0]
+
+        res.status(201).json({
+            status:'success',
+            data: {restaurant}
+        })
+
+    } 
+    catch (error) 
+    {
+        console.log(error)   
+    }
 })
 
-app.put('/api/v1/restaurants/:id',(req,res) => 
+app.put('/api/v1/restaurants/:id',async (req,res) => 
 {
-    const {id:restaurantsID} = req.params
-    const {body} = req
-    res.status(200).json({restaurantsID,body})
+    try 
+    {
+        const {id:restaurantID} = req.params
+        const {name,location,price_range} = req.body
+
+        if(!name || !location || !price_range)
+            throw new Error("Bad Request")
+
+        const result = await query('UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *',
+        [name,location,price_range,restaurantID]
+        )
+        const restaurant = result.rows[0]
+
+        res.status(200).json({
+            status:'success',
+            data: {restaurant}
+        })
+    } 
+    catch (error) 
+    {
+        console.log(error)
+    }
 })
 
-app.delete('/api/v1/restaurants/:id',(req,res) => 
+app.delete('/api/v1/restaurants/:id', async (req,res) => 
 {
-    const {id:restaurantsID} = req.params
-    res.status(200).json({msg: `delete restaurants with id ${restaurantsID}`})
+    try 
+    {
+        const {id:restaurantsID} = req.params
+
+        const result = await query(`DELETE FROM restaurants WHERE id = $1`,[restaurantsID])
+
+
+        res.status(200).json({
+            status:'success',
+        })
+        
+    } 
+    catch (error) 
+    {
+        console.log(error)
+        
+    }
 })
 
 
